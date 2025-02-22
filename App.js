ee72b1297d2043505994c3cc5b3e332151395ad7
Azimjon Mirzoev <155828956+AzimMirzoev@users.noreply.github.com>
2025-02-22 22:31:47 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Alert, Pressable, Text, View } from 'react-native';
+import Main from './src/components/Main';
+import AppBar from './src/components/AppBar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+	<View>
+	  <AppBar />
+	  <Main />
+	</View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
